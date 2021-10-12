b2cbabfc5040c93701411ded7942fc6ae7ab0e96
Madhav Jha <madhavjha@gmail.com>
2021-10-12 16:43:43 -0700

    just creating a vault which can hold token and assigning a PDA as owner

--- tests/gm-coin.js
+++ tests/gm-coin.js
@@ -1,14 +1,40 @@
 const anchor = require('@project-serum/anchor');
+const serumCmn = require("@project-serum/common");
+const { TOKEN_PROGRAM_ID } = require("@solana/spl-token");
 
 describe('gm-coin', () => {
 
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.Provider.env());
 
+
+
   it('Is initialized!', async () => {
     // Add your test here.
     const program = anchor.workspace.GmCoin;
-    const tx = await program.rpc.initialize();
+    const [mint, originalVault] = await serumCmn.createMintAndVault(
+      program.provider,
+      new anchor.BN(1_000_000)
+    );
+    const vault = anchor.web3.Keypair.generate();
+
+    let [vaultProgram, nonce] = await anchor.web3.PublicKey.findProgramAddress(
+      [vault.publicKey.toBuffer()],
+      program.programId
+    )
+    const tx = await program.rpc.initialize({
+      signers: [vault],
+      instructions: [
+        ...(await serumCmn.createTokenAccountInstrs(
+          program.provider,
+          vault.publicKey,
+          mint,
+          vaultProgram
+        ))
+      ]
+    });
     console.log("Your transaction signature", tx);
   });
+
+
 });
