a423b51fdd88852bae963f0ea4c239caf44f3acf
Madhav Jha <madhavjha@gmail.com>
2021-10-13 16:42:36 -0700

    initial

--- app/src/MainApp.tsx
+++ app/src/MainApp.tsx
@@ -1,373 +1,96 @@
 import React from 'react';
 import { AnchorWallet, useAnchorWallet } from '@solana/wallet-adapter-react';
+import { Button } from '@material-ui/core';
 const serumCmn = require("@project-serum/common");
+
+const IDL = require("./gm_coin.json");
 
 const anchor = require("@project-serum/anchor");
 const { SystemProgram, Connection } = anchor.web3;
 
+const connectionUrl = 'https://api.devnet.solana.com';
+const INITIAL_MINT_AMOUNT = 1_000_000;
+const COOLOFF_SECONDS = 30;
 
-const IDL = {
-    "version": "0.0.0",
-    "name": "xbasic_1",
-    "instructions": [
-        {
-            "name": "introduceYourself",
-            "accounts": [
-                {
-                    "name": "payer",
-                    "isMut": false,
-                    "isSigner": true
-                },
-                {
-                    "name": "visitor",
-                    "isMut": false,
-                    "isSigner": true
-                },
-                {
-                    "name": "visitorState",
-                    "isMut": true,
-                    "isSigner": false
-                },
-                {
-                    "name": "systemProgram",
-                    "isMut": false,
-                    "isSigner": false
-                }
-            ],
-            "args": [
-                {
-                    "name": "visitorBump",
-                    "type": "u8"
-                }
-            ]
+const createMint = async (
+    anchorWallet: AnchorWallet | undefined,
+    appState: any,
+    setAppState: any,
+    setPrintableAppState: any) => {
+    const provider = new anchor.Provider(
+        new Connection(connectionUrl),
+        anchorWallet,
+        anchor.Provider.defaultOptions(),
+    );
+    const programId = new anchor.web3.PublicKey(IDL.metadata.address);
+    const program = new anchor.Program(IDL, programId, provider);
+
+    const [_mint, _originalVault] = await serumCmn.createMintAndVault(
+        program.provider,
+        new anchor.BN(INITIAL_MINT_AMOUNT)
+    );
+    setPrintableAppState({
+        mint: _mint.toString(),
+        originalVault: _originalVault.toString(),
+    });
+    return;
+    const _vault = anchor.web3.Keypair.generate();
+
+    let [_vaultProgram, _nonce] = await anchor.web3.PublicKey.findProgramAddress(
+        [_vault.publicKey.toBuffer()],
+        program.programId
+    )
+
+    const [_pda, _bump] = await anchor.web3.PublicKey.findProgramAddress(
+        [Buffer.from(anchor.utils.bytes.utf8.encode("gm_coin"))],
+        program.programId
+    );
+    const cooloffSeconds = new anchor.BN(COOLOFF_SECONDS);
+    const tx = await program.rpc.initialize(_bump, cooloffSeconds, {
+        accounts: {
+            globalState: _pda,
+            payer: program.provider.wallet.publicKey,
+            systemProgram: anchor.web3.SystemProgram.programId,
         },
-        {
-            "name": "visit",
-            "accounts": [
-                {
-                    "name": "visitor",
-                    "isMut": false,
-                    "isSigner": true
-                },
-                {
-                    "name": "visitorState",
-                    "isMut": true,
-                    "isSigner": false
-                }
-            ],
-            "args": []
+        signers: [_vault],
+        instructions: [
+            ...(await serumCmn.createTokenAccountInstrs(
+                program.provider,
+                _vault.publicKey,
+                _mint,
+                _vaultProgram
+            ))
+        ]
+    });
+    console.log("Your transaction signature", tx);
+    setAppState({
+        ...appState,
+        mint: _mint,
+        originalVault: _originalVault,
+        vault: _vault,
+        vaultProgram: _vaultProgram,
+        nonce: _nonce,
+    })
+    setPrintableAppState({
+        mint: _mint.toString(),
+        originalVault: _originalVault.toString(),
+        vault: {
+            publicKey: _vault.publicKey.toString(),
+            secretKey: "🤫",
         },
-        {
-            "name": "initialize",
-            "accounts": [
-                {
-                    "name": "myAccount",
-                    "isMut": true,
-                    "isSigner": true
-                },
-                {
-                    "name": "user",
-                    "isMut": true,
-                    "isSigner": true
-                },
-                {
-                    "name": "systemProgram",
-                    "isMut": false,
-                    "isSigner": false
-                }
-            ],
-            "args": [
-                {
-                    "name": "data",
-                    "type": "u64"
-                }
-            ]
-        },
-        {
-            "name": "update",
-            "accounts": [
-                {
-                    "name": "myAccount",
-                    "isMut": true,
-                    "isSigner": false
-                }
-            ],
-            "args": [
-                {
-                    "name": "data",
-                    "type": "u64"
-                }
-            ]
-        },
-        {
-            "name": "createCheck",
-            "accounts": [
-                {
-                    "name": "check",
-                    "isMut": true,
-                    "isSigner": false
-                },
-                {
-                    "name": "vault",
-                    "isMut": true,
-                    "isSigner": false
-                },
-                {
-                    "name": "checkSigner",
-                    "isMut": false,
-                    "isSigner": false
-                },
-                {
-                    "name": "from",
-                    "isMut": true,
-                    "isSigner": false
-                },
-                {
-                    "name": "to",
-                    "isMut": false,
-                    "isSigner": false
-                },
-                {
-                    "name": "owner",
-                    "isMut": false,
-                    "isSigner": false
-                },
-                {
-                    "name": "tokenProgram",
-                    "isMut": false,
-                    "isSigner": false
-                }
-            ],
-            "args": [
-                {
-                    "name": "amount",
-                    "type": "u64"
-                },
-                {
-                    "name": "memo",
-                    "type": {
-                        "option": "string"
-                    }
-                },
-                {
-                    "name": "nonce",
-                    "type": "u8"
-                }
-            ]
-        },
-        {
-            "name": "cashCheck",
-            "accounts": [
-                {
-                    "name": "check",
-                    "isMut": true,
-                    "isSigner": false
-                },
-                {
-                    "name": "vault",
-                    "isMut": true,
-                    "isSigner": false
-                },
-                {
-                    "name": "checkSigner",
-                    "isMut": false,
-                    "isSigner": false
-                },
-                {
-                    "name": "to",
-                    "isMut": true,
-                    "isSigner": false
-                },
-                {
-                    "name": "owner",
-                    "isMut": false,
-                    "isSigner": true
-                },
-                {
-                    "name": "tokenProgram",
-                    "isMut": false,
-                    "isSigner": false
-                }
-            ],
-            "args": []
-        }
-    ],
-    "accounts": [
-        {
-            "name": "Check",
-            "type": {
-                "kind": "struct",
-                "fields": [
-                    {
-                        "name": "from",
-                        "type": "publicKey"
-                    },
-                    {
-                        "name": "to",
-                        "type": "publicKey"
-                    },
-                    {
-                        "name": "amount",
-                        "type": "u64"
-                    },
-                    {
-                        "name": "memo",
-                        "type": {
-                            "option": "string"
-                        }
-                    },
-                    {
-                        "name": "vault",
-                        "type": "publicKey"
-                    },
-                    {
-                        "name": "nonce",
-                        "type": "u8"
-                    },
-                    {
-                        "name": "burned",
-                        "type": "bool"
-                    }
-                ]
-            }
-        },
-        {
-            "name": "MyAccount",
-            "type": {
-                "kind": "struct",
-                "fields": [
-                    {
-                        "name": "data",
-                        "type": "u64"
-                    }
-                ]
-            }
-        },
-        {
-            "name": "VisitorState",
-            "type": {
-                "kind": "struct",
-                "fields": [
-                    {
-                        "name": "visitCount",
-                        "type": "u64"
-                    },
-                    {
-                        "name": "bump",
-                        "type": "u8"
-                    }
-                ]
-            }
-        }
-    ],
-    "errors": [
-        {
-            "code": 300,
-            "name": "InvalidCheckNonce",
-            "msg": "The given nonce does not create a valid program derived address."
-        },
-        {
-            "code": 301,
-            "name": "InvalidCheckSigner",
-            "msg": "The derived check signer does not match that which was given."
-        },
-        {
-            "code": 302,
-            "name": "AlreadyBurned",
-            "msg": "The given check has already been burned."
-        },
-        {
-            "code": 303,
-            "name": "InvalidMessage",
-            "msg": "Sorry that doesn't look like a GM message."
-        }
-    ],
-    "metadata": {
-        "address": "2j4NMzDYQPLpS2HKLR7EnzPt5MXBt3fT9PeWTvUAznQp"
-    }
+        vaultProgram: _vaultProgram.toString(),
+        _nonce
+    })
 }
 
 export default function MainApp() {
     const anchorWallet = useAnchorWallet();
-    const [mint, setMint] = React.useState(null);
-    const [god, setGod] = React.useState(null);
-    const doIt = async (anchorWallet: AnchorWallet) => {
-        console.log("doing it");
-        const provider = new anchor.Provider(
-            new Connection('https://api.devnet.solana.com'),
-            anchorWallet,
-            anchor.Provider.defaultOptions(),
-        );
-        const programId = new anchor.web3.PublicKey(IDL.metadata.address);
-        const program = new anchor.Program(IDL, programId, provider);
-        console.log({ program });
-        const myAccount = anchor.web3.Keypair.generate();
-        const [visitorState, visitorBump] = await anchor.web3.PublicKey.findProgramAddress(
-            [provider.wallet.publicKey.toBuffer(), "1"],
-            program.programId
-        );
-
-        // const resp = await program.rpc.introduceYourself(new anchor.BN(visitorBump),
-        //     {
-        //         accounts: {
-        //             payer: provider.wallet.publicKey,
-        //             visitor: provider.wallet.publicKey,
-        //             visitorState: visitorState,
-        //             systemProgram: anchor.web3.SystemProgram.programId
-        //         },
-        //     }
-        // );
-        // console.log({ resp });
-        async function visit() {
-            await provider.connection.confirmTransaction(
-                await program.rpc.visit(
-                    {
-                        accounts: {
-                            visitor: provider.wallet.publicKey,
-                            visitorState: visitorState
-                        },
-                    }
-                ),
-                "finalized"
-            );
-        }
-
-        console.log("About to visit again, this takes a while for solana to finalize...");
-        await visit();
-        let visitorStateAccount = await program.account.visitorState.fetch(visitorState)
-        console.log({ visitorStateAccount })
-        await program.rpc.initialize(new anchor.BN(1234), {
-            accounts: {
-                myAccount: myAccount.publicKey,
-                user: provider.wallet.publicKey,
-                systemProgram: SystemProgram.programId,
-            },
-            signers: [myAccount],
-        });
-    }
-    React.useEffect(() => {
-
-        if (anchorWallet) {
-            // doIt(anchorWallet);
-        }
-    }, [anchorWallet])
+    const [appState, setAppState] = React.useState({});
+    const [printableAppState, setPrintableAppState] = React.useState({});
     return <>
-        <div>hello</div>
-        <div>{JSON.stringify(mint)}</div>
-        <div>{JSON.stringify(god)}</div>
-        <button onClick={async () => {
-            const provider = new anchor.Provider(
-                new Connection('https://api.devnet.solana.com'),
-                anchorWallet,
-                anchor.Provider.defaultOptions(),
-            );
-            const [mint, god] = await serumCmn.createMintAndVault(
-                provider,
-                new anchor.BN(1_000_000)
-            );
-            setMint(mint);
-            setGod(god);
-        }}>Create and mint</button>
+        <div>Hello</div>
+        <div>{JSON.stringify(printableAppState)}</div>
+        {anchorWallet && <Button variant="outlined" color="secondary" onClick={async () => await createMint(anchorWallet, appState, setAppState, setPrintableAppState)}>Mint</Button>}
 
     </>
 }
